b5a133ab1e3739fc9c06713ebdfae69e16e30915
I_CAN_PLAY <miracle678067@gmail.com>
2018-05-18 22:25:36 +0800

    first commit

---
+++ src/main/sql/schema.sql
@@ -0,0 +1,37 @@
+--创建数据库脚本
+CREATE  DATABASE seckill;
+--使用数据库
+use seckill;
+--创建秒杀库存表
+CREATE TABLE seckill(
+  `seckill_id` bigint NOT NULL AUTO_INCREMENT COMMENT '商品库存id',
+  `name` varchar (120) NOT NULL COMMENT '商品名称',
+  `number` int NOT NULL COMMENT '库存数量',
+  `start_time` timestamp NOT NULL COMMENT '秒杀开启时间',
+  `end_time` timestamp NOT NULL COMMENT '秒杀结束时间',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY(seckill_id),
+  key idx_start_time(start_time),
+  key idx_end_time(end_time),
+  key idx_create_time(create_time)
+)ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='秒杀库存表';
+
+--初始化数据
+insert into
+  seckill(name,number,start_time,end_time)
+  values
+    ('1元秒杀坚果tNT工作站',100,'2018-06-01 00:00:00','2018-06-02 00:00:00'),
+    ('1元秒杀iphonex',100,'2018-06-01 00:00:00','2018-06-02 00:00:00'),
+    ('1元秒杀坚果3',100,'2018-06-01 00:00:00','2018-06-02 00:00:00'),
+    ('1元秒杀mac',100,'2018-06-01 00:00:00','2018-06-02 00:00:00');
+
+--秒杀成功明细表
+--用户登录认证的相关的信息
+CREATE TABLE success_killed(
+  `seckill_id` bigint NOT NULL COMMENT '秒杀商品id',
+  `user_phone` bigint NOT NULL COMMENT '用户手机号',
+  `state` bigint NOT NULL DEFAULT -1 COMMENT '状态表示：-1：无效，0：成功，1：已付款',
+  `create_time` timestamp NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (seckill_id,user_phone),
+  key idx_create_time(create_time)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='秒杀成功明细表';
